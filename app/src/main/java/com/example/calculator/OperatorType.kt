5a02e1c5cd501f954c0b4937a93bd040ab373f13
Swieta Nurjanah Hetty <88220816+Swieta06@users.noreply.github.com>
2023-02-22 14:04:21 +0700

    first commit

---
+++ app/src/main/java/com/example/calculator/OperatorType.kt
@@ -0,0 +1,9 @@
+package com.example.calculator
+
+object OperatorType {
+    //tambah
+    const val TAMBAH=1
+    const val KURANG=2
+    const val KALI=3
+    const val BAGI=4
+}
---
+++ app/src/main/java/com/example/calculator/person.kt
@@ -0,0 +1,14 @@
+package com.example.calculator
+
+class person {
+    val name="swieta"
+    var age=20
+
+    fun change(){
+        age=30
+        val persenage=age
+
+
+    }
+
+}
